2aa0daf8eaf3747cee3612831fd44a5faebcb4f4
zhangs16 <36863638+zhangs16@users.noreply.github.com>
2018-05-01 13:14:42 -0400

    finish sign in and sign up Page

--- CSE201/LogIn.php
+++ CSE201/LogIn.php
@@ -13,23 +13,23 @@ if ($conn->connect_error) {
     die("Fail to connect to database:" . $conn->connect_error);
 }
 
-$email = $_POST['email'];
-
 $username = $_POST['username'];
 
 $password = $_POST['password'];
 
-$birthday = $_POST['birthday'];
+$sql = "Select username, pswd from Users where username = '$username' and pswd = '$password'";
+$result = $conn->query($sql);
 
-$sql = "INSERT INTO Users (email, username, pswd, bdate) VALUES ('$email', '$username', '$password', '$birthday')";
-
-if ($conn->query($sql) === TRUE) {
+if ($result->num_rows > 0) {
     $conn->close();
-    echo "Adding into database successfully.";
+    echo "Sign In Successfully!";
     header('Refresh:3,Url=HomePage.html');
     die;
 } else {
+    $conn->close();
     echo "Error: " . $sql . "<br>" . $conn->error;
+    header('Refresh:3,Url=SignInPage.html');
+    die;
 }
 
 $conn->close();
